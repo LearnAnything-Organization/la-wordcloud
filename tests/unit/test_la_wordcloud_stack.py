e85305c1d78400c6bffb392a9a0c63c68a376fa2
Anderson Gomes <54241354+Nosredzzz@users.noreply.github.com>
2024-08-04 13:03:22 -0400

    Fix references

--- tests/unit/test_la_wordcloud_stack.py
+++ tests/unit/test_la_wordcloud_stack.py
@@ -1,14 +1,16 @@
 import aws_cdk as core
 import aws_cdk.assertions as assertions
 
-from la_wordcloud.la_wordcloud_stack import LAWordCloudStack
+from aws_wordcloud.aws_wordcloud_stack import AWSWordCloudStack
+
 
 # example tests. To run these tests, uncomment this file along with the example
-# resource in la_wordcloud/la_wordcloud_stack.py
+# resource in aws_wordcloud/aws_wordcloud_stack.py
 def test_sqs_queue_created():
     app = core.App()
-    stack = LAWordCloudStack(app, "la-wordcloud")
+    stack = AWSWordCloudStack(app, "la-wordcloud")
     template = assertions.Template.from_stack(stack)
+
 
 #     template.has_resource_properties("AWS::SQS::Queue", {
 #         "VisibilityTimeout": 300
